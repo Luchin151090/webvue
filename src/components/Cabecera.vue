9ffb40c15cef1e33fccd335b7aef337866852a42
Luchin151090 <lgonzalesu@ulasalle.edu.pe>
2024-04-24 23:07:51 -0500

    1er componente

---
+++ src/components/Cabecera.vue
@@ -0,0 +1,28 @@
+<template>
+ <div>
+  <b-nav>
+    <b-nav-item active>Active</b-nav-item>
+    <b-nav-item>Link</b-nav-item>
+    <b-nav-item>Another Link</b-nav-item>
+    <b-nav-item disabled>Disabled</b-nav-item>
+  </b-nav>  
+</div>
+</template>
+<script>
+
+</script>
+<style>
+div {
+    background-color: rgb(127, 127, 182);
+    margin-left: 0px;
+    margin-right: 100px;
+    padding: 2px;
+    
+}
+b-nav-item{
+    margin-left: 10px;
+    font-size: 25px;
+}
+
+
+</style>
